38c520f96fcb52df60af80dff197a7b0c4162685
Henrik <henrik_mathiesen@hotmail.com>
2016-03-26 18:01:13 +0100

    -post todo working

--- app/directives/yt-add-edit-todo.directive.js
+++ app/directives/yt-add-edit-todo.directive.js
@@ -8,7 +8,7 @@ angular
             replace: true,
             scope: {},
             templateUrl: 'app/templates/yt-add-edit-todo.template.html',
-            controller: function () {
+            controller: function (apiFactory) {
                 var ctrl = this;
                 
                 ctrl.todo = {
@@ -17,7 +17,10 @@ angular
                     text: ""
                 };
                 
-                console.log(ctrl);
+                ctrl.postTodo = function () {
+                    console.log(ctrl.todo);
+                    apiFactory.postTodo(ctrl.todo);
+                };
             },
             controllerAs: 'ctrl',
             bindToController: true
