772ecabc7177dca431918e1d3ad3c43364f2e3ad
Henrik <henrik_mathiesen@hotmail.com>
2016-03-27 20:39:51 +0200

    -block icons -devider border

--- app/directives/yt-todos.directive.js
+++ app/directives/yt-todos.directive.js
@@ -9,12 +9,12 @@ angular
            scope: {},
            template: [
                '<div ng-repeat="td in ctrl.todos | orderBy: ctrl.sortOrder | filter:{ label: ctrl.todosFilter.label, text: ctrl.todosFilter.text }" class="row yt-no-side-margins {{ td.label | getCssClassForLabelFilter }}">',
-                    '<div class="col-sm-8">',
+                    '<div class="col-sm-10">',
                         '<p><span>{{ td.date | date: "short" }}</span> {{ td.text }}</p>',
                     '</div>',
-                    '<div class="col-sm-4">',
-                        '<span class="fa fa-3x fa-times yt-fa-icon-delete--block"></span>',
-                        '<span class="fa fa-3x fa-pencil yt-fa-icon-edit--block"></span>',
+                    '<div class="col-sm-2 yt-no-padding-right">',
+                        '<span class="fa fa-3x fa-times yt-fa-icon-delete--block yt-border-divider-left-sm"></span>',
+                        '<span class="fa fa-3x fa-pencil yt-fa-icon-edit--block yt-border-divider-top-xs yt-border-divider-left-sm"></span>',
                     '</div>',
                '</div>'
            ].join(''),
