787957a7223ed23b29a0b0f44ce1b6d084fce250
Henrik <henrik_mathiesen@hotmail.com>
2016-03-27 21:21:44 +0200

    -delete todo

--- app/directives/yt-todos.directive.js
+++ app/directives/yt-todos.directive.js
@@ -8,25 +8,43 @@ angular
            replace: true,
            scope: {},
            template: [
-               '<div ng-repeat="td in ctrl.todos | orderBy: ctrl.sortOrder | filter:{ label: ctrl.todosFilter.label, text: ctrl.todosFilter.text }" class="row yt-no-side-margins {{ td.label | getCssClassForLabelFilter }}">',
-                    '<div class="col-sm-10">',
-                        '<p><span class="thin-text">{{ td.date | date: "short" }}&nbsp;&nbsp;&nbsp;</span> {{ td.text }}</p>',
+               '<div>',
+                    '<div ng-repeat="td in ctrl.todos | orderBy: ctrl.sortOrder | filter:{ label: ctrl.todosFilter.label, text: ctrl.todosFilter.text }" class="row yt-no-side-margins {{ td.label | getCssClassForLabelFilter }}">',
+                        '<div class="col-sm-10">',
+                            '<p><span class="thin-text">{{ td.date | date: "short" }}&nbsp;&nbsp;&nbsp;</span> {{ td.text }}, id: {{ td.id }}</p>',
+                        '</div>',
+                        '<div class="col-sm-2 yt-no-padding-right">',
+                            '<span class="fa fa-3x fa-times yt-fa-icon-delete--block yt-border-divider-left-sm" ng-click="ctrl.deleteTodo(td)"></span>',
+                            '<span class="fa fa-3x fa-pencil yt-fa-icon-edit--block yt-border-divider-top-xs yt-border-divider-left-sm"></span>',
+                        '</div>',
                     '</div>',
-                    '<div class="col-sm-2 yt-no-padding-right">',
-                        '<span class="fa fa-3x fa-times yt-fa-icon-delete--block yt-border-divider-left-sm"></span>',
-                        '<span class="fa fa-3x fa-pencil yt-fa-icon-edit--block yt-border-divider-top-xs yt-border-divider-left-sm"></span>',
-                    '</div>',
-               '</div>'
+                '</div>'
            ].join(''),
-           controller: function ($filter) {
+           controller: function ($filter, apiFactory, getSetErrorFactory) {
                var ctrl = this;
                ctrl.sortOrder = "-date";
                console.log(ctrl.todos);
+               
+               var deleteTodoSuccess = function () {
+                   console.log("DELETE SUCCESS");
+                   ctrl.reloadCallback();
+               };
+               
+               var deleteTodoError = function () {
+                   getSetErrorFactory.setError(true);
+               };
+               
+               ctrl.deleteTodo = function (todo) {
+                   apiFactory.deleteTodo(todo.id)
+                        .then(deleteTodoSuccess)
+                        .catch(deleteTodoError);
+               };
            },
            controllerAs: 'ctrl',
            bindToController: {
                todos: '=',
-               todosFilter: '='
+               todosFilter: '=',
+               reloadCallback: '='
            }
        };
     });
