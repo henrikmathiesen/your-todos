f62c74de2fdbc2dc57bcdc681d0e68f064fd7307
Henrik <henrik_mathiesen@hotmail.com>
2016-04-07 20:46:08 +0200

    -unit tests

---
+++ tst/services/get-set-error.factory.test.js
@@ -0,0 +1,26 @@
+/// <reference path="../../typings/tsd.d.ts" />
+
+describe("get-set-errorFactory should communicate if app is in an error state or not", function() {
+
+    beforeEach(module('main'));
+
+    var getSetErrorFactory;
+
+    beforeEach(inject(function(_getSetErrorFactory_) {
+        getSetErrorFactory = _getSetErrorFactory_;
+    }));
+    
+    it("is created with isError set to false", function () {
+        expect(getSetErrorFactory.getError()).toBe(false);
+    });
+    
+    it("has a method for setting if app is in an error state, takes a boolean", function () {
+        expect(getSetErrorFactory.setError).toBeDefined(); 
+    });
+    
+    it("has a method for getting if app is in an error state, returns a boolean", function () {
+        getSetErrorFactory.setError(true);
+        expect(getSetErrorFactory.getError()).toBe(true);
+    });
+
+});
