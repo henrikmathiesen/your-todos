a16b8b84fc2a183f9bc15ecb502e527cac1d7a15
Henrik <henrik_mathiesen@hotmail.com>
2016-04-16 22:09:03 +0200

    -unit tests $httpBackend

--- tst/services/api.factory.test.js
+++ tst/services/api.factory.test.js
@@ -4,43 +4,47 @@ describe("api factory makes ajax calls to a node server for CRUD operations", fu
 
     var $httpBackend;
     var apiFactory;
-    var apiMock;
 
     beforeEach(module('main'));
 
     beforeEach(inject(function (_$httpBackend_, _apiFactory_) {
         $httpBackend = _$httpBackend_;
         apiFactory = _apiFactory_;
-
-        apiMock = [
-            {
-                date: '2016-03-23T13:00:00.000Z',
-                id: 0,
-                label: 'project',
-                text: 'My todo about some project'
-            },
-            {
-                date: '2016-04-16T10:00:00.000Z',
-                id: 1,
-                label: 'work',
-                text: 'My todo about some work'
-            }
-        ]
     }));
 
-    it("should be defined", function () {
-        expect(apiFactory).toBeDefined();
-        expect($httpBackend).toBeDefined();
-    });
-
-    it("should expose a getTodos method that makes an ajax call getting todos", function () {
-        $httpBackend.when('GET', '/api/todos').respond(200, apiMock);
-
-        apiFactory.getTodos().then(function (response) {
-            expect(response.data.length).toBe(apiMock.length);
-        });
-
+    afterEach(function () {
         $httpBackend.flush();
     });
 
+    it("should expose a getTodos method that makes an ajax call getting todos from back end", function () {
+        $httpBackend.when('GET', '/api/todos').respond(200, [{ id: 1 }]);
+
+        apiFactory.getTodos().then(function (response) {
+            expect(response.data.length).toBe(1);
+        });
+    });
+
+    it("should expose a postTodo method that makes an ajax call posting a todo to back end, back end returns the id of posted todo", function () {
+        $httpBackend.when('POST', '/api/todo').respond(200, 1);
+
+        apiFactory.postTodo({ text: "To do today ..." }).then(function (response) {
+            expect(response.data).toBe(1);
+        });
+    });
+    
+    it("should expose a putTodo method that makes an ajax call putting a todo to back end", function () {
+        $httpBackend.when('PUT', '/api/todo/' + '1').respond(200);
+
+        apiFactory.putTodo(1, { id: 1 }).then(function (response) {
+            expect(response.status).toBe(200);
+        });
+    });
+    
+    it("should expose a deleteTodo method that makes an ajax call deleting a todo to back end", function () {
+        $httpBackend.when('DELETE', '/api/todo/' + '1').respond(200);
+
+        apiFactory.deleteTodo(1).then(function (response) {
+            expect(response.status).toBe(200);
+        });
+    });
 });
