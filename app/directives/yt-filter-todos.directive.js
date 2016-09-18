7c6e4c604aba1a3268de81b906bb010f56a64ebf
Henrik <henrik_mathiesen@hotmail.com>
2016-09-18 13:51:35 +0200

    -seperate template file for filter -datepicker widget for filtering by date

--- app/directives/yt-filter-todos.directive.js
+++ app/directives/yt-filter-todos.directive.js
@@ -7,24 +7,7 @@ angular
             restrict: 'E',
             replace: true,
             scope: {},
-            template: [
-                '<div class="row">',
-                    '<div class="col-sm-3">',
-                        '<select class="yt-form-input" ng-model="ctrl.todosFilter.label">',
-                            '<option value="" selected="true">All</option>',
-                            '<option value="{{ctrl.LABELS.work}}">Work</option>',
-                            '<option value="{{ctrl.LABELS.joy}}">Joy</option>',
-                            '<option value="{{ctrl.LABELS.project}}">Project</option>',
-                        '</select>',
-                    '</div>',
-                    '<div class="col-sm-5">',
-                        '<input class="yt-form-input" type="text" ng-model="ctrl.todosFilter.text" placeholder="Search ..." />',
-                    '</div>',
-                    '<div class="col-sm-4">',
-                        '<input class="yt-form-input" type="date" ng-model="ctrl.todosFilter.date" />',
-                    '</div>',
-                '</div>'
-            ].join(''),
+            templateUrl:'yt-filter-todos.template.html',
             controller: function (LABELS_CONSTANTS) {
                 var ctrl = this;
                 ctrl.LABELS = LABELS_CONSTANTS;
