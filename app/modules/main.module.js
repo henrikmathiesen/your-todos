a7ce41cced6e804bc823288f56920b00e1f9627e
Henrik <henrik_mathiesen@hotmail.com>
2016-09-20 04:26:00 +0200

    -applied fast click

--- app/modules/main.module.js
+++ app/modules/main.module.js
@@ -1,4 +1,8 @@
 /// <reference path="../../typings/tsd.d.ts" />
 
+$(function() {
+    FastClick.attach(document.body);
+});
+
 angular
     .module('main', ['templatecache', 'ui.bootstrap.datetimepicker']);
